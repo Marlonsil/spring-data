fcbda6cbd4936a59f796e07e97a49a39a0ac4d02
Marlonsil <marlonsilveira397@gmail.com>
2024-04-23 13:35:08 +0000

    Modelo Genero

---
+++ app/src/main/java/application/controller/GeneroCotroller.java
@@ -0,0 +1,8 @@
+package application.controller;
+  
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.controller;
+import ore.springframework.web.bind.annotation.requestMapping;
+public application.repository.GeneroRepositoty {
+    
+}
